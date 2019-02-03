fc813380d82e0902e041e96c91b68887c754c51f
Bence Pimpli <pimplibence@gmail.com>
2019-02-03 21:20:07 +0100

    Add tslint.json && recommended idea Project.xml config for code-formating

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,33 @@
-import { ConnectedRouter, connectRouter, routerMiddleware } from 'connected-react-router'
+import { ConnectedRouter, connectRouter, routerMiddleware } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router';
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
-import logger from 'redux-logger';
+import reduxLogger from 'redux-logger';
 import './index.scss';
 import { HomeScreen } from './screens/home.screen';
 
-// this history will be syncronised with store
+const root$ = document.getElementById('application');
 const history = createBrowserHistory();
 
+const reducers = {
+    router: connectRouter(history)
+};
+
 const store = createStore(
-    combineReducers({
-        // Router reducer, do not rename it (property)
-        router: connectRouter(history)
-    }),
+    combineReducers(reducers),
     compose(
-        // this middleware will syncronise history and store
         applyMiddleware(routerMiddleware(history)),
-        applyMiddleware(logger)
+        applyMiddleware(reduxLogger)
     )
 );
 
 ReactDOM.render(<Provider store={store}>
     <ConnectedRouter history={history}>
         <Switch>
-            <Route path={'/'} exact component={HomeScreen}/>
+            <Route path={'/'} exact={true} component={HomeScreen}/>
         </Switch>
     </ConnectedRouter>
-</Provider>, document.getElementById('application'));
+</Provider>, root$);
--- src/libs/connect.ts
+++ src/libs/connect.ts
@@ -1,15 +1,13 @@
 import { connect as _connect } from 'react-redux';
 
 type MapStateProps = (state: any) => {
-    [key: string]: any
+    [key: string]: any,
 };
 
 type MapDispatchProps = (dispatch: (action: any) => void) => {
-    [key: string]: ((...args: any) => void) | (() => void)
+    [key: string]: ((...args: any) => void) | (() => void),
 };
 
 export const connect = (mapStateProps: MapStateProps, mapDispatchProps: MapDispatchProps, mergeProps?: any): any => {
-    return _connect(mapStateProps, mapDispatchProps, mergeProps || null, {
-        shouldHandleStateChanges: false
-    })
+    return _connect(mapStateProps, mapDispatchProps, mergeProps || null, { shouldHandleStateChanges: false });
 };
