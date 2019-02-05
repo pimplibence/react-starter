3ecc48c497708bb99f3bc996543cc7ce17878755
Bence Pimpli <pimplibence@gmail.com>
2019-02-05 21:16:42 +0100

    Update connection lib & fix tslint

--- src/libs/connect.ts
+++ src/libs/connect.ts
@@ -1,13 +1,14 @@
 import { connect as _connect } from 'react-redux';
+import { Action } from 'redux';
 
 type MapStateProps = (state: any) => {
     [key: string]: any,
 };
 
-type MapDispatchProps = (dispatch: (action: any) => void) => {
+type MapDispatchProps = (dispatch: (action: Action<any>) => void) => {
     [key: string]: ((...args: any) => void) | (() => void),
 };
 
-export const connect = (mapStateProps: MapStateProps, mapDispatchProps: MapDispatchProps, mergeProps?: any): any => {
+export const connect = (mapStateProps?: MapStateProps, mapDispatchProps?: MapDispatchProps, mergeProps?: any): any => {
     return _connect(mapStateProps, mapDispatchProps, mergeProps || null, { shouldHandleStateChanges: false });
 };
