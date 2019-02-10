6947af868cbca3677ab87c6993b8ffc304f5ef3e
Bence Pimpli <pimplibence@gmail.com>
2019-02-11 00:42:03 +0100

    WIP

--- src/screens/uikit.screen.tsx
+++ src/screens/uikit.screen.tsx
@@ -10,13 +10,15 @@ export class UikitScreen extends React.Component<any, any> {
     public render() {
         return <div className="container">
 
-            <h1>H1 Heading</h1>
-            <h2>H2 Heading</h2>
-            <h3>H3 Heading</h3>
-            <h4>H4 Heading</h4>
-            <h5>H5 Heading</h5>
-            <h6>H6 Heading</h6>
-            <p>Paragraph</p>
+            <h1>H1 Heading Text</h1>
+            <h2>H2 Heading Text</h2>
+            <h3>H3 Heading Text</h3>
+            <h4>H4 Heading Text</h4>
+            <h5>H5 Heading Text</h5>
+            <h6>H6 Heading Text</h6>
+            <p>Paragraph Text</p>
+            <span>Span Text</span>
+            <small>Small Text</small>
 
         </div>;
     }
