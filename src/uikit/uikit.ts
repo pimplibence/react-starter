4776bf36860386bbb48768c85e0a287be76261b5
Pimpli Bence <pimplibence@gmail.com>
2019-03-06 22:03:14 +0100

    Define component list

--- src/uikit/uikit.ts
+++ src/uikit/uikit.ts
@@ -1,3 +1,42 @@
-export abstract class UiKit {
+// tslint:disable variable-name
+
+export abstract class Uikit {
+
+    /**
+     * Text input
+     * Number input
+     * Email input
+     * Password input
+     */
+    public static Input = () => 'Component';
+
+    /**
+     * Textarea input
+     * @constructor
+     */
+    public static Textarea = () => 'Component';
+
+    /**
+     * Select input
+     * Multi Select input
+     * Search Single input
+     * Multi select input
+     * OnCreate Single input
+     * OnCreate Multi Select input
+     * @constructor
+     */
+    public static Select = () => 'Component';
+
+    /**
+     * Simple Button
+     * @constructor
+     */
+    public static Button = () => 'Component';
+
+    /**
+     * Badge / Tag
+     * @constructor
+     */
+    public static Badge = () => 'Component';
 
 }
